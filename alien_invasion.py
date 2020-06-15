003ec12a75abb7e04881f11bb9b4a4e605d45a2a
godofshaorma <yosimitubogdan@gmail.com>
2020-06-15 22:54:15 +0300

    Added first alien ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+
 
 class AlienInvasion:
     """Overall class to manage game behavior and assets"""
@@ -25,6 +27,10 @@ class AlienInvasion:
         self.ship=Ship(self)
 
         self.bullets=pygame.sprite.Group()
+
+        self.aliens=pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -78,12 +84,19 @@ class AlienInvasion:
             new_bullet=Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        #Make an alien
+        alien=Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
